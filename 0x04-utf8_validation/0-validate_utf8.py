95dbf1976c0f11da480ec8b72cea7d02b129cf75
Madu-f0r0 <duherbert@gmail.com>
2024-04-07 19:08:53 +0100

    Completed UTF-8 validation script

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Contains the definition of the function `validUTF8`"""
+
+
+def validUTF8(data):
+    """Determines if a given data set represents a valid UTF-8 encoding"""
+    for character in data:
+        if character > 255:
+            return False
+    return True
