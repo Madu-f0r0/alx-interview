fb101c4a0dba32d998ba99ddd1fa8dfd69e30c68
Madu-f0r0 <duherbert@gmail.com>
2024-04-19 07:53:26 -0700

    Fixed random order of character names using recursive call stack

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,20 +5,28 @@ const request = require('request');
 const movieId = process.argv[2];
 const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
+function requestCharacter (characterUrlList, idx) {
+  if (idx === characterUrlList.length) {
+    return;
+  }
+
+  request(characterUrlList[idx], (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+
+      requestCharacter(characterUrlList, idx + 1);
+    }
+  });
+}
+
 request(movieUrl, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
     const charactersUrls = JSON.parse(body).characters;
-    charactersUrls.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          const characterName = JSON.parse(body).name;
-          console.log(characterName);
-        }
-      });
-    });
+    requestCharacter(charactersUrls, 0);
   }
 });
