37a2791898f34b62627de658b8f417ebb9f53541
Madu-f0r0 <duherbert@gmail.com>
2024-05-03 13:23:19 +0100

    Completed the change making function

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module contains the definition of the `makeChange` method
+"""
+
+
+def makeChange(coins, total):
+    """Returns the fewest number of specified coins required to
+    make change for a given amount
+    """
+    coins.sort(reverse=True)
+    noOfCoins = 0
+
+    if total <= 0:
+        return -1
+    for coin in coins:
+        while total >= coin:
+            noOfCoins += int(total / coin)
+            total %= coin
+
+    if total > 0:
+        return -1
+    return noOfCoins
